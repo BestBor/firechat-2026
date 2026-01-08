fc50b66eb69e43c6b7a46704b9a37a2bfb441394
BestBor <185581264+BestBor@users.noreply.github.com>
2026-01-07 23:41:55 -0500

    feat: register page (v1)

--- src/pages/auth/login.page.tsx
+++ src/pages/auth/login.page.tsx
@@ -1,10 +1,45 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import CardFooterAuth from "@/components/card-footer-auth"
 import { useAuthActions } from "@/hooks/use-auth-action"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { loginZodSchema, type LoginZodSchemaType } from "@/lib/zod.schema"
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle,  } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
 const LoginPage = () => {
 
-  const { loading } = useAuthActions()
+  const { loading, login } = useAuthActions()
+
+  const form = useForm<LoginZodSchemaType>({
+    resolver: zodResolver(loginZodSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  const onSubmit = async(data: LoginZodSchemaType) => {
+    const response = await login(data)
+    if (!response.success) {
+      if (response.error?.code === "auth/invalid/login-credentials") {
+        form.setError("email", {
+          type: "manual",
+          message: "Invalid email or password"
+        })
+      }
+    }
+  }
 
   return (
     <Card>
@@ -15,7 +50,55 @@ const LoginPage = () => {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        Stuff...
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Enter your email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder=""
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              className="w-full"
+              disabled={loading}
+            >
+              {loading ? "Checking In": "Login"}
+            </Button>
+          </form>
+
+        </Form>
       </CardContent>
       <CardFooterAuth
         type="login"
@@ -24,4 +107,5 @@ const LoginPage = () => {
     </Card>
   )
 }
+
 export default LoginPage
--- src/pages/auth/register.page.tsx
+++ src/pages/auth/register.page.tsx
@@ -1,28 +1,143 @@
 import CardFooterAuth from "@/components/card-footer-auth"
+import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import { useAuthActions } from "@/hooks/use-auth-action"
+import { registerZodSchema, type RegisterZodSchemaType } from "@/lib/zod.schema"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { toast } from "sonner"
+
 
 
 const RegisterPage = () => {
 
-  const { loading } = useAuthActions()
+  const { register, loading } = useAuthActions()
+
+  const form = useForm<RegisterZodSchemaType>({
+    resolver: zodResolver(registerZodSchema),
+    mode: "onChange",
+    defaultValues: {
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    }
+  })
+  
+  const onSubmit = async (values: RegisterZodSchemaType) => {
+    const response = await register(values)
+    if (response.error) {
+      toast.error("There is a problem creating the new account")
+      console.log(response.error.code)
+      if (response.error.code === "auth/email-already-in-use") {
+        form.setError("email", {
+          type: "manual",
+          message: "Email is already in use",
+        })
+      } else {
+        console.error("Registration error: ", response.error)
+      }
+    } else {
+      console.log("Registration successful", values);
+    }
+  }
 
   return (
-    <Card>
+    <Card className="bg-white">
       <CardHeader>
         <CardTitle>Register</CardTitle>
-        <CardDescription>
-          Register your account using Email and Password or with Google
-        </CardDescription>
+        <CardDescription>Create a new account</CardDescription>
       </CardHeader>
       <CardContent>
-        Stuff...
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8"
+          >
+            <FormField
+              control={form.control}
+              name="displayName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Display Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Enter your display name"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Enter your email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      {...field}
+                      placeholder="Enter your password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      {...field}
+                      placeholder="Confirm your password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              className="w-full"
+              disabled={loading}
+            >
+              Register
+            </Button>
+          </form>
+        </Form>
       </CardContent>
       <CardFooterAuth
         type="register"
         loading={loading}
       />
     </Card>
-  )
+  );
 }
+
 export default RegisterPage
